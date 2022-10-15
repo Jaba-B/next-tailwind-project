17811da5664dc2d1c62b3bb40cae9fb7167db00e
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-10-15 10:12:46 +0400

    add layout(header and footer) for every page

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className='bg-indigo-800 text-orange-300 p-7 text-center  text-lg'>
+            Thanks For Visiting
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+function Header() {
+    return (
+        <div className='bg-indigo-800 text-orange-300 p-7 text-lg'>
+            <ul className='flex justify-center'>
+                <li className='px-6 hover:text-neutral-100'><Link href='#'> Clubs </Link></li>
+                <li className='px-6 hover:text-neutral-100'><Link href='#'> Pokemons </Link></li>
+                <li className='px-6 hover:text-neutral-100'><Link href='#'> Feedbacks </Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
